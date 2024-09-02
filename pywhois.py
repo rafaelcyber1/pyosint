6e19b33d1af3f27b455971923cdce4962b7aae72
Rafael <168600337+rafaelcyber1@users.noreply.github.com>
2024-09-01 21:12:53 -0300

    Delete pywhois.py

--- pywhois.py
+++
@@ -1,7 +0,0 @@
-import whois
-print("*************************")
-print("instagram: @rafael_cyber1")
-print("*************************")
-dominio = input("Dominio: ")
-consulta_whois = whois.whois(dominio)
-print(consulta_whois.text)
